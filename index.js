b6a397193864c9e5b771d649886611e79a37d432
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-11-13 14:29:33 +0600

    find method or read all data function created

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
 
+const uri =
+  "mongodb+srv://books-db:FNyqY9sHCsiL4VsG@cluster0.syvrcr2.mongodb.net/?appName=Cluster0";
 
-app.get('/', (req, res) => {
-  res.send('Hello World hi!')
-})
-app.get('/hello', (req, res) => {
-  res.send('Hello are you?')
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    await client.connect();
 
+    const db = client.db("books-db");
+    const bookCollection = db.collection("books");
+
+    //find
+    //findOne
+    app.get("/books", async (req, res) => {
+      const result = await bookCollection.find().toArray();
+      //console.log(result)
+
+      res.send(result);
+    });
+
+   
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World hi!");
+});
+app.get("/hello", (req, res) => {
+  res.send("Hello are you?");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
